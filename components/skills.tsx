3fa7f29d973edbbcce313bb6d6d721ee9dd71b3d
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-06-21 14:45:19 +0700

    rea

--- components/skills.tsx
+++ components/skills.tsx
@@ -14,7 +14,7 @@ export const Skilss = () => {
                     <img src="./React-icon.svg.png" alt="reac" />
                   </a>
                   <a href="#" className="py-3 max-w-10">
-                    <img src="./tailwind.svg" alt="reac" />
+                    <img src="./tailwind.svg" alt="rea" />
                   </a>
                   <a href="#" className="py-4 max-w-10">
                     <img src="./Vue.js_Logo_2.svg.png" alt="react" />
