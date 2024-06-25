58a5683d398e83ca8f400c9c08edbfb3fda02165
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-06-25 21:27:08 +0700

    Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,17 +11,17 @@ export const Footer = () => {
                 </div>
                 <div className="w-full px-4 md:w-1/3 mt-10">
                     <h3 className="text-3xl font-bold mb-3 ">Follow Me On</h3>
-                    <ul className="font-medium tracking-[2px]">
-                        <li><a href="#">Github</a></li>
-                        <li><a href="#">instagram</a></li>
+                    <ul className="font-medium tracking-[2px] underline">
+                        <li><a href="https://github.com/HapidzGIT">Github</a></li>
+                        <li><a href="https://www.instagram.com/hafidzfrqn_/">instagram</a></li>
                         <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Dribble</a></li>
-                        <li><a href="#">Twitter</a></li>
+                        <li><a href="https://dribbble.com/hafid_11">Dribble</a></li>
+                        <li><a href="https://twitter.com/Muhamma28299736">Twitter</a></li>
                     </ul>
                 </div>
                 <div className="w-full px-4 md:w-1/3 mt-10">
                     <h3 className="text-4xl font-bold mb-3">Links</h3>
-                    <ul className="font-medium tracking-[2px]">
+                    <ul className="font-medium tracking-[2px] underline">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#skills">Skills</a></li>
@@ -52,7 +52,7 @@ export const Footer = () => {
       </a>
                 </div>
             </div>
-            <h3 className="text-center pt-5">Create With 💖 By Muhammad Hafidz Furqon</h3>
+            <h3 className="text-center pt-5 text-xl text-slate-300">Create With 💖 By Muhammad Hafidz Furqon</h3>
         </div>
         </section>
         </>
