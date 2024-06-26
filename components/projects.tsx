e1c75b5e5f94eb5973a2c09cfba982b5105f70a2
Muhammad Hafidz <143271950+HapidzGIT@users.noreply.github.com>
2024-06-26 19:58:54 +0700

    adding particles

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,12 +12,14 @@ export const Project = () => {
             </div>
           </div>
           <div className="flex flex-wrap w-full px-4 justify-center gap-8" data-aos="fade-up" data-aos-duration="1000">
-          <div className="mb-12 lg:w-1/3 border border-primary shadow-primary rounded-md shadow" data-aos="fade-up" data-aos-duration="1000">
+            <div className="mb-12 lg:w-1/3 border border-primary shadow-primary rounded-md shadow" data-aos="fade-up" data-aos-duration="1000">
               <div className="rounded-md overflow-hidden">
                 <img src="./classmeet-SI.png" className=" sm:h-full" alt="foto-kelas" width="w-full" />
               </div>
               <h3 className="font-bold mb-3 mt-[1.5rem] text-2xl px-2">Information System Classmeet</h3>
-              <p className="font-extralight leading-relaxed tracking-[2px] px-2">I made this project as a final assignment from my school to create something useful for the school. Finally I thought about creating this information system.</p>
+              <p className="font-extralight leading-relaxed tracking-[2px] px-2">
+                I made this project as a final assignment from my school to create something useful for the school. Finally I thought about creating this information system.
+              </p>
               <div className="flex gap-x-2 px-2">
                 <div className="py-4 max-w-16 grayscale hover:grayscale-0 transition duration-300 ease-in-out">
                   <img src="./React-icon.svg.png" alt="react" />
@@ -38,7 +40,9 @@ export const Project = () => {
                 <img src="/Foto-kelas.png" className="h-full" alt="foto-kelas" width="w-full" />
               </div>
               <h3 className="font-bold mb-3 mt-4 text-2xl px-2">My Class Website</h3>
-              <p className="font-extralight leading-relaxed tracking-[2px] px-2">This is my class website that I have created with my friends, this website aims to introduce our class to visitors as well as a place for collecting data and student projects.</p>
+              <p className="font-extralight leading-relaxed tracking-[2px] px-2">
+                This is my class website that I have created with my friends, this website aims to introduce our class to visitors as well as a place for collecting data and student projects.
+              </p>
               <div className="flex gap-x-2 px-2 py-2">
                 <div className="py-4 max-w-16 grayscale hover:grayscale-0 transition duration-300 ease-in-out">
                   <img src="./React-icon.svg.png" alt="react" />
@@ -93,13 +97,16 @@ export const Project = () => {
             </div>
             <div className="mb-12 lg:w-1/3 border border-primary shadow-primary rounded-md shadow" data-aos="fade-up" data-aos-duration="1000">
               <div className="rounded-md overflow-hidden ">
-                <img src="./UIKA-3.png" className="h-full" alt="foto-kelas" width="w-full" />
+                <img src="./news.png" className="h-full" alt="foto-kelas" width="w-full" />
               </div>
-              <h3 className="font-bold mb-3 mt-5 text-2xl px-2">University Web Design</h3>
+              <h3 className="font-bold mb-3 mt-5 text-2xl px-2">List worldwide news </h3>
               <p className="font-extralight leading-relaxed tracking-[2px] px-2">My First Commercial Design for one of the universities in the city of Bogor and really helped improve my design skills, because I learned from the project.</p>
               <div className="flex gap-x-2 px-2">
-                <div className="py-4 max-w-16 grayscale hover:grayscale-0 transition duration-300 ease-in-out px-3">
-                  <img src="./1667px-Figma-logo.svg.png" alt="react" />
+                <div className="py-4 max-w-16 grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+                  <img src="./React-icon.svg.png" alt="react" />
+                </div>
+                <div className="py-3 max-w-16 grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+                  <img src="./tailwind.svg" alt="react" />
                 </div>
               </div>
               <div className="my-4 mx-3 mb-7">
